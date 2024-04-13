a1bae72fa2abb175f08124d79d20cb83519c63c8
lijunweiz <takealeaf@yeah.net>
2024-04-13 09:44:23 +0800

    config

--- bee-manage/src/main/java/cn/unminded/bee/manage/BeeManage.java
+++ bee-manage/src/main/java/cn/unminded/bee/manage/BeeManage.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("cn.unminded.bee.persistence.mapper")
+@MapperScan("cn.unminded.bee.persistence")
 @SpringBootApplication(scanBasePackages = "cn.unminded.bee")
 public class BeeManage {
 
